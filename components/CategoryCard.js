71512d1ac7ef16842db33ef7c4b93c98542b557c
unknown <jeancarlosayala57@gmail.com>
2022-12-25 23:22:07 -0600

    make rating in Category card

--- components/CategoryCard.js
+++ components/CategoryCard.js
@@ -1,7 +1,9 @@
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 import React from 'react';
+import { StarIcon } from 'react-native-heroicons/solid';
+import { starColor } from '../styles/colors';
 
-export const CategoryCard = ({imgUrl, title}) => {
+export const CategoryCard = ({ imgUrl, title }) => {
   return (
     <TouchableOpacity className='relative mr-2'>
       <Image className='w-20 h-20 rounded' source={{
@@ -12,17 +14,27 @@ export const CategoryCard = ({imgUrl, title}) => {
   )
 };
 
-export const RestaurantCard = ({title, restauranData}) => {
-  return(
-    <View>
-      <Text>{title}</Text>
-
+export const RestaurantCard = ({ restauranData }) => {
+  return (
+    <View className='flex-row pt-3'>
       {
-        restauranData.map(({title}) =>{
-          return(
-            <View>
-              <Text>{title}</Text>
-            </View>
+        restauranData.map(({ title, id, imgUrl, rating, genre }) => {
+          return (
+            <TouchableOpacity className='mr-3' key={id}>
+              <Image className='w-64 h-36 rounded-sm ' source={{
+                uri: imgUrl
+              }} />
+              <View className='px-3 pb-4 pt-2'>
+                <Text className='font-bold text-lg'>{title}</Text>
+              </View>
+              <View className='flex-row items-center'>
+                <StarIcon color={starColor} opacity={0.5} />
+                <Text className='text-xs text-gray-500'>
+                  <Text className=''>{rating}</Text>
+                  - {genre}
+                </Text>
+              </View>
+            </TouchableOpacity>
           )
         })
       }
