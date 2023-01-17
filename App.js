59e1157b8188bb458aa1fdb7266a46b9858e4639
Jeancarlosayala28 <carlos.ayala@puntocommerce.com>
2023-01-16 23:27:09 -0600

    new routes, basket and restaurant

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { store } from './store';
+import { Provider } from 'react-redux';
+
 import HomeScreen from './screens/HomeScreen';
+import RestaurantScreen from './screens/RestaurantScreen';
+import BasketScreen from './screens/BasketScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='Home' component={HomeScreen} />
-      </Stack.Navigator>
+      <Provider store={store}>
+        <Stack.Navigator>
+          <Stack.Screen name='Home' component={HomeScreen} />
+          <Stack.Screen name='Restaurant' component={RestaurantScreen} />
+          <Stack.Screen name='Basket' component={BasketScreen} 
+          options={{
+            presentation: 'modal',
+            headerShown: false
+          }} />
+        </Stack.Navigator>
+      </Provider>
     </NavigationContainer>
   );
 }
