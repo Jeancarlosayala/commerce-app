df615b1f7c2b8864dba3004af96a9ed09c2fcbc7
Jeancarlosayala28 <carlos.ayala@puntocommerce.com>
2023-01-21 20:16:14 -0600

    new views delivery screen and payment screen and new packages

--- App.js
+++ App.js
@@ -7,19 +7,31 @@ import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
 import RestaurantScreen from './screens/RestaurantScreen';
 import BasketScreen from './screens/BasketScreen';
+import PreparingOrderScreen from './screens/PreparingOrderScreen';
+import PaymentScreen from './screens/PaymentScreen';
+
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+
   return (
     <NavigationContainer>
       <Provider store={store}>
         <Stack.Navigator>
+          <Stack.Screen name='Payments' component={PaymentScreen} options={{
+            presentation: 'fullScreenModal',
+            headerShown: false
+          }} />
           <Stack.Screen name='Home' component={HomeScreen} />
           <Stack.Screen name='Restaurant' component={RestaurantScreen} />
-          <Stack.Screen name='Basket' component={BasketScreen} 
-          options={{
-            presentation: 'modal',
+          <Stack.Screen name='Basket' component={BasketScreen}
+            options={{
+              presentation: 'modal',
+              headerShown: false
+            }} />
+          <Stack.Screen name='PreparingOrder' component={PreparingOrderScreen} options={{
+            presentation: 'fullScreenModal',
             headerShown: false
           }} />
         </Stack.Navigator>
