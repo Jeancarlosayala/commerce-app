7751eec88f00f21a24037f0fe7959049e886adfd
unknown <jeancarlosayala57@gmail.com>
2023-01-24 22:15:27 -0600

    new function add caracter

---
+++ utils/creditCard.js
@@ -0,0 +1,13 @@
+
+export const agregarCaracter = (cadena, caracter, pasos) => {
+  let cadenaConCaracteres = "";
+  const longitudCadena = cadena.length;
+  for (let i = 0; i < longitudCadena; i += pasos) {
+    if (i + pasos < longitudCadena) {
+      cadenaConCaracteres += cadena.substring(i, i + pasos) + caracter;
+    } else {
+      cadenaConCaracteres += cadena.substring(i, longitudCadena);
+    }
+  }
+  return cadenaConCaracteres;
+}
