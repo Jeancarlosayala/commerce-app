0a2ef57de197f06c4676434ef4e8a10981705466
unknown <jeancarlosayala57@gmail.com>
2023-01-24 22:14:17 -0600

    New color

--- styles/colors.js
+++ styles/colors.js
@@ -1,5 +1,6 @@
 export const primaryColor = '#9B59B6';
 export const purple_1 = '#AF7AC5';
+export const purple_2 = '#bc6fdddc';
 export const grayColor = 'gray';
 export const starColor = '#FFC300';
 export const grayColor1 = '#D7DBDD';
